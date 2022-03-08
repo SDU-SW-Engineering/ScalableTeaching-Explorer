04f8e13e0b3f719c0e7b648ef107c690a4a9c564
Niels Faurskov <niels@faurskov.dev>
2022-03-08 11:14:30 +0100

    Cleanup and some additional stuff

--- src/extension.ts
+++ src/extension.ts
@@ -1,68 +1,27 @@
 import * as vscode from 'vscode';
-import Axios from 'axios';
 import { Courses } from './courses';
 import { GradingTree } from './trees/grading/gradingTree';
+import { DocumentProvider } from './documentProvider';
+import openCourse from './commands/openCourse';
+import signIn from './commands/signIn';
+import { DocumentViewer } from './documentViewer';
 
 
 export function activate(context: vscode.ExtensionContext) {
-
-
-	console.log('Loaded');
 	vscode.commands.executeCommand('setContext', 'scalableteaching.authenticated', context.globalState.get('authenticated') === true);
+	vscode.workspace.registerTextDocumentContentProvider("scalable", new DocumentProvider);
+	const documentViewer = new DocumentViewer();
+	documentViewer.start();
 	
-	let view = vscode.window.createTreeView('scalable.courses', {
+	
+	/*let view = vscode.window.createTreeView('scalable.courses', {
 		"treeDataProvider": new Courses
 	});
-	view.title = "Updated!";
+	//view.title = "Updated!";*/
 
-	vscode.commands.registerCommand('scalableteaching.openCourse', async(id) => {
-		let test = await vscode.window.showQuickPick(new Promise((resolve, reject) => {
-			setTimeout(() => resolve(["hello"]), 2000);
-		}), {
-			"title": "Open course...",
-		});
-		if (test === undefined)
-			return;
-		vscode.commands.executeCommand("setContext", "scalableteaching.openedCourse", 11);
-		
-	//	vscode.window.showInformationMessage("hey " + id);
-	//	vscode.window.registerTreeDataProvider("scalable.feedback.list", new GradingTree);
-	});
+	vscode.commands.registerCommand('scalableteaching.openCourse', openCourse);
 
-	let signInCommand = vscode.commands.registerCommand('scalableteaching.signIn', async () => {
-		vscode.window.withProgress({
-			"location": vscode.ProgressLocation.Notification,
-			"cancellable": false,
-			"title": "Attempting to sign you in..."
-		}, async(progress) => {
-			const token = (Math.random() + 1).toString(36).substring(2);
-			vscode.env.openExternal(vscode.Uri.parse(`http://localhost:8080/vs-code/authenticate?token=${token}`));
-			let tries = 0;
-			while (tries < 3)
-			{
-				await new Promise(r => setTimeout(r, 3000));
-				console.log("Sign in attempt: " + (tries+1));
-				try
-				{
-					let response = await Axios.get(`http://localhost:8080/vs-code/retrieve-authentication?token=${token}`);
-					console.log(response.data);
-					context.globalState.update('token' , response.data.token);
-					context.globalState.update('name', response.data.name);
-					context.globalState.update('authenticated', true);
-					vscode.commands.executeCommand('setContext', 'scalableteaching.test', false);
-					vscode.window.showInformationMessage("Authenticated.");
-					
-
-					return;
-				} catch (ignored){}
-				tries++;
-			}	
-			vscode.window.showErrorMessage("Authentication timed-out.");
-		});
-		
-		
-
-	});
+	let signInCommand = vscode.commands.registerCommand('scalableteaching.signIn', () => signIn(context));
 
 	context.subscriptions.push(signInCommand);
 }
@@ -70,4 +29,5 @@ export function activate(context: vscode.ExtensionContext) {
 
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+}
